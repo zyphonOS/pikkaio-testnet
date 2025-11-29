138187e6e89b299e6c2eb44a4c73e5f53fefa2d6
zyphonOS <zyphon.os.you@gmail.com>
2025-11-29 16:48:26 +0100

    Fix build for deployment

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,7 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/pikkaio-testnet' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/pikkaio-testnet/' : '',
+  }
 }
 
 module.exports = nextConfig
