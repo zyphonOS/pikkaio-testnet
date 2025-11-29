974f2718fd3fe9be2697cad26f996c90f7bcb930
zyphonOS <zyphon.os.you@gmail.com>
2025-11-29 05:21:41 +0100

    Configure for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,9 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true
-  }
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/pikkaio-testnet' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/pikkaio-testnet/' : '',
 }
 
 module.exports = nextConfig
